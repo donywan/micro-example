caf398fc0f0b81b3d6e0ea39311dcb43aeeec840
donywan <donywan@outlook.com>
2020-04-17 08:17:38 +0800

    Change fegin client name

--- micro-classroom/src/main/java/com/micro/classroom/fegin/UserService.java
+++ micro-classroom/src/main/java/com/micro/classroom/fegin/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "service-provider",fallback = UserServiceFallback.class)
+@FeignClient(name = "micro-user",fallback = UserServiceFallback.class)
 public interface UserService {
     @GetMapping("/api/user")
     String getUsers();

c6b8716c175f77ec22d1a83e46fc206cc4898e22
donywan <donywan@outlook.com>
2020-04-25 11:07:16 +0800

    Update Readme.

--- micro-user/src/main/java/com/micro/user/UserApplication.java
+++ micro-user/src/main/java/com/micro/user/UserApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,10 +14,11 @@ import java.sql.SQLException;
 @EnableAutoConfiguration
 //@NacosConfigurationProperties(dataId = "nacos-spring", autoRefreshed = true)
 @EntityScan(value = "com.micro.domain.entity")
+@RefreshScope
 public class UserApplication {
 
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(UserApplication.class, args);
 //        String test = configurableApplicationContext.getEnvironment().getProperty("test.test");
 //        System.out.printf("test的数据" + test);
